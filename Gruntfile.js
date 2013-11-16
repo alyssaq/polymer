7a383bd8ca8edae1acbcab522fee50476a3ab246
alyssaq <alyssaquek@gmail.com>
2013-11-16 21:35:52 +0800

    Removed the use of matchdep npm module

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all grunt tasks
-  //require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   require('time-grunt')(grunt);
 
   require('load-grunt-config')(grunt, {
