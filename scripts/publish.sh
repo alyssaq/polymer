ac00ba088a49c51b4733436495c6df2c89296bfb
alyssaq <alyssaquek@gmail.com>
2013-11-16 14:09:59 +0800

    Edited script to update gh-pages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-#Publish the files contained in app to gh-pages
-rm -rf app/
+pushd .
+
+#Clear app directory
+rm -rf app/*
 
 #build the app using grunt. app folder is created
 grunt build
 
-#delete gh-pages branch
-git push origin --delete gh-pages   
+#Change to directory app
+cd app
+
+#commit any changes in app directory 
+git commit -am "gh-pages"
 
 #push the files from app folder to gh-pages
-git subtree push --prefix app origin gh-pages  
+git push origin gh-pages
+
+popd
+
